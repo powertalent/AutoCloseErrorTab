1e00c90641852eca719ae8d1cf6c95e640c86427
powertalent <powertalentstars@gmail.com>
2024-06-02 20:32:47 +0700

    add icon, move file

--- background.js
+++
@@ -1,8 +0,0 @@
-function checkTabTitle(tabId, changeInfo, tabInfo) {
-    if (changeInfo.title && changeInfo.title === "Server Not Found") {
-      browser.tabs.remove(tabId);
-    }
-  }
-  
-  browser.tabs.onUpdated.addListener(checkTabTitle);
-  
